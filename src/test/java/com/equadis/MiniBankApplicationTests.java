9749043087ab0dcc1258115d43da8016ca849903
ANIMASHAUN HABEEB <animashaunhabeeb@ANIMASHAUNs-MBP.Home>
2024-02-21 01:42:58 +0000

    Implementing Testing

--- src/test/java/com/equadis/MiniBankApplicationTests.java
+++ src/test/java/com/equadis/MiniBankApplicationTests.java
@@ -1,13 +1,36 @@
 package com.equadis;
 
+import com.equadis.bank.service.CustomerService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@WebMvcTest
 class MiniBankApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private CustomerService customerService;
+
 	@Test
-	void contextLoads() {
+	public void shouldCreateNewCustomer()
+	{
+
+	}
+
+	@Test
+	public void shouldCreateNewBankAccount(){
+
+	}
+
+	@Test
+	public void shouldCreateNewTransaction(){
+
 	}
 
 }
